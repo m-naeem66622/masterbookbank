727d3a36801a144868b5cf2c2ebeb6a4197458a3
Muhammad Naeem <m.naeem66622@outlook.com>
2023-06-23 00:01:56 +0500

    bootstrap customization setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,19 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./styles/style.css";
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.js";
+import "./scss/_custom.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
